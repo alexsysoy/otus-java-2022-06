7b29bb9ef4340877ce18397fd6636d87e624a4a7
alexsysoy <alexsysoy@yandex.ru>
2022-10-27 21:16:14 +0300

    Проблема с update не решена

--- hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -4,14 +4,25 @@ package ru.otus.crm.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.List;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
-@Table(name = "client")
+@ToString
 public class Client implements Cloneable {
 
     @Id
@@ -21,6 +32,16 @@ public class Client implements Cloneable {
 
     @Column(name = "name")
     private String name;
+
+    @OneToOne(cascade = {CascadeType.PERSIST},
+            fetch = FetchType.EAGER,
+            orphanRemoval = true)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private List<Phone> phones;
 
     public Client(String name) {
         this.id = null;
@@ -32,16 +53,15 @@ public class Client implements Cloneable {
         this.name = name;
     }
 
-    @Override
-    public Client clone() {
-        return new Client(this.id, this.name);
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
     }
 
     @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+    public Client clone() {
+        return new Client(this.id, this.name, this.address.clone(), this.phones);
     }
 }
---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,34 @@
+package ru.otus.crm.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Phone implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    public Phone(Long id, String number) {
+        this.id = null;
+        this.number = number;
+    }
+
+    @Override
+    public Phone clone() {
+        return new Phone(this.id, this.number);
+    }
+}
