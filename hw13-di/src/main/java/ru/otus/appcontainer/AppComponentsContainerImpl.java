48c2aed9209c111fffa65c58b93be4d6abac638b
alexsysoy <alexsysoy@yandex.ru>
2022-11-08 18:19:37 +0300

    Добавил основной вариант домашней работы

--- hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,15 +1,20 @@
 package ru.otus.appcontainer;
 
-import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
+import ru.otus.exception.AppException;
 
-import java.io.*;
-import java.lang.annotation.Annotation;
-import java.net.URL;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
@@ -22,21 +27,88 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        System.out.println("GO!!!");
+        var configMethods = new PriorityQueue<Method>(Comparator.comparing(o -> o.getAnnotation(AppComponent.class).order()));
 
-        var classLoader = Thread.currentThread().getContextClassLoader();
-        System.out.println("CLASSLOADER: " + classLoader.getName());
+        Arrays.stream(configClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .forEach(configMethods::add);
 
-        var packages = Thread.currentThread().getContextClassLoader().getDefinedPackages();
+        fillComponents(configMethods, createConfigInstance(configClass));
+    }
 
-        for (Package aPackage : packages) {
-            System.out.println("PACKAGE: " + aPackage.getName());
-            for (Class aClass : findAllClassesUsingClassLoader(aPackage.getName())) {
-                System.out.println("CLASS: " + aClass.getName());
-                System.out.println("ANNOTATION: " + Arrays.asList(aClass.getDeclaredAnnotations()));
-                System.out.println("METHOD: " + Arrays.asList(aClass.getMethods()));
+    private void fillComponents(PriorityQueue<Method> methods, Object configInstance) {
+        Method configMethod;
+        Object[] args;
+        while (!methods.isEmpty()) {
+            configMethod = methods.poll();
+            args = new Object[configMethod.getParameterCount()];
+
+            var params = configMethod.getParameters();
+            for (int i = 0; i < configMethod.getParameterCount(); i++) {
+                args[i] = getComponent(params[i].getType());
+            }
+            createComponentInContext(configInstance, configMethod, args);
+        }
+    }
+
+    private Object createConfigInstance(Class<?> configClass) {
+        try {
+            return configClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createComponentInContext(Object configInstance, Method configMethod, Object... args) {
+        try {
+           Object componentInstance;
+            componentInstance = configMethod.invoke(configInstance, args);
+            var componentName = configMethod.getAnnotation(AppComponent.class).name();
+            checkNotDuplicateComponentName(componentName);
+
+            if (isContainMoreThenOneElement(componentInstance)) {
+                Iterator<Object> iterator = appComponents.iterator();
+                Object temp;
+                while (iterator.hasNext()) {
+                    temp = iterator.next();
+                    if (temp.getClass() == componentInstance.getClass()) {
+                        iterator.remove();
+                    }
+                }
+                System.out.println("FFF" + appComponents);
+            } else {
+                appComponents.add(componentInstance);
+                appComponentsByName.put(componentName, componentInstance);
+            }
+
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void checkNotDuplicateComponentName(String name) {
+        if (appComponentsByName.containsKey(name)) {
+            throw new AppException(String.format("Component with name %s is present in the context", name));
+        }
+    }
+
+    private boolean isContainMoreThenOneElement(Object componentInstance) {
+        for (Object component : appComponents) {
+            if (component.getClass() == componentInstance.getClass()) {
+                return true;
             }
         }
+        return false;
+    }
+
+    private Object getComponent(Class<?> clazz) {
+        for (Object component : appComponents) {
+            if (Arrays.asList(component.getClass().getInterfaces()).contains(clazz)) {
+                return component;
+            }
+        }
+        throw new AppException("Get component exception");
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -46,32 +118,26 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        for (Object component : appComponents) {
+            if (Arrays.stream(component.getClass().getInterfaces()).anyMatch(inter -> inter == componentClass)) {
+                return (C) component;
+            }
+            if (component.getClass() == componentClass) {
+                return (C) component;
+            }
+        }
+        throw new AppException(String.format("There isn't component with class: %s", componentClass.getName()));
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(String componentName) {
-        return null;
-    }
-
-    public Set<Class> findAllClassesUsingClassLoader(String packageName) {
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        return reader.lines()
-                .filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, packageName))
-                .collect(Collectors.toSet());
-    }
-
-    private Class getClass(String className, String packageName) {
-        try {
-            return Class.forName(packageName + "."
-                    + className.substring(0, className.lastIndexOf('.')));
-        } catch (ClassNotFoundException e) {
-            // todo handle exception
+        var result = appComponentsByName.get(componentName);
+        if (result != null) {
+            return (C) result;
         }
-        return null;
+        throw new AppException(String.format("There isn't component with name: %s", componentName));
     }
 }
